fc411a273f2371ddeee6fbad7de01d9ae720f03e
DmitriyPanfilov <126699728+DmitriyPanfilov@users.noreply.github.com>
2023-10-04 22:12:31 +0300

    Task 3

---
+++ task3.java
@@ -0,0 +1,46 @@
+package lesson2.homework;
+//Исходный код:
+//public static void main(String[] args) throws Exception {
+//        try {
+//        int a = 90;
+//        int b = 3;
+//        System.out.println(a / b);
+//        printSum(23, 234);
+//        int[] abc = { 1, 2 };
+//        abc[3] = 9;
+//        } catch (Throwable ex) {
+//        System.out.println("Что-то пошло не так...");
+//        } catch (NullPointerException ex) {
+//        System.out.println("Указатель не может указывать на null!");
+//        } catch (IndexOutOfBoundsException ex) {
+//        System.out.println("Массив выходит за пределы своего размера!");
+//        }
+//        }
+//public static void printSum(Integer a, Integer b) throws FileNotFoundException {
+//        System.out.println(a + b);
+//        }
+
+
+
+
+public class task3 {
+    public static void main(String[] args) {
+        try {
+            int a = 90;
+            int b = 9;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = { 1, 2 };
+            abc[2] = 9;
+        } catch (NullPointerException e) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        } catch (Throwable e) {
+            System.out.println("Что-то пошло не так...");
+        }
+    }
+    public static void printSum(Integer a, Integer b) {
+        System.out.println(a + b);
+    }
+}
